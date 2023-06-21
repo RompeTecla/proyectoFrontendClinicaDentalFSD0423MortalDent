2dd2f118e55176604cd996056cbef0a6a65733b6
RompeTecla <javalinogames@gmail.com>
2023-06-21 11:16:58 +0200

    FEAT: Home folder and its .css and .jsx are created. The Home folder is relocated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-import './App.css'
+import React from "react";
+import { Header } from "./common/Header/Header";
+import { Body } from "./common/Body/Body";
+import { Footer } from "./common/Footer/Footer";
+// import './App.css'
 
 function App() {
   return (
     <div>
-
+      <Header />
+      <Body />
+      <Footer />
     </div>
   )
-}
+};
 
 export default App
