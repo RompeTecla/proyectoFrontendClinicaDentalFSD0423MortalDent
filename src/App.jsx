753e5ec372435dff10f0b8daa1da74d8e13ec20e
RompeTecla <javalinogames@gmail.com>
2023-06-26 00:36:15 +0200

    FIX&FEAT: Reworked the common NavBar from scratch and the common Navigator for navigation between views.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
-import CustomNavbar from './common/CustomNavbar/CustomNavbar'
-// import { CustomNavbar } from './common/Header/Header'
-// import { Body } from './pages/Body/Body'
+import { NavBar } from './common/Navbar/Navbar'
+
 
 function App() {
 
   return (
     <>
-      <CustomNavbar />
-      <div>Hola</div>
-      <Body />
+      <NavBar />
     </>
   )
 }
---
+++ src/common/Navbar/NavBar.jsx
@@ -0,0 +1,25 @@
+import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import "./Navbar.css";
+import { Navigator } from "../Navigator/Navigator";
+
+export const NavBar = () => {
+  return (
+
+    <Navbar className="all-nav">
+      <div className="row-nav">
+        <div className="mortaldent">
+          <div className="link-title">MORTALDENT</div>
+        </div>
+        <>
+          <div className="nav-init custom-nav-init">
+            <Navigator ruta={"Inicio"} destino={"/"} />
+            <Navigator ruta={"Tratamientos"} destino={"/treatments"} />
+            <Navigator ruta={"Registro"} destino={"/register"} />
+            <Navigator ruta={"Inicio sesion"} destino={"/login"} />
+          </div>
+        </>
+      </div>
+    </Navbar>
+  );
+};
