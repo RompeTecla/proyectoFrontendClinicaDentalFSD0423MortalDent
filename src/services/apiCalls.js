0afe80c5051763261ab49b2dce60d2653e024e5d
RompeTecla <javalinogames@gmail.com>
2023-07-13 03:40:36 +0200

    FIX: Added endpoints for the correct connection of axios with the API db tables bringUsers, appointmentsAll, bookAppointments, bringAppointments and updateAppointment for the moment.

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,11 +1,61 @@
-import axios from 'axios'
+import axios from "axios";
 
-const root = "http://localhost:3000/"
+const root = "http://localhost:3000/";
 
-export const loginMe = async(body) => {
-    return await axios.post(`${root}login`, body)
-}
+export const loginMe = async (body) => {
+  return await axios.post(`${root}login`, body);
+};
 
 export const registerUser = async (body) => {
-    return await axios.post(`${root}users/reg`, body)
-  }
+  return await axios.post(`${root}users/reg`, body);
+};
+
+export const bringUsers = async (token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  return await axios.get(`${root}users/all`, config);
+};
+
+export const appointmentsAll = async (token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  return await axios.get(`${root}appointments/doctor`, config);
+};
+
+export const bookAppointment = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  return await axios.post(`${root}appointments`, body, config);
+};
+
+export const bringAppointments = async (token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  return await axios.get(`${root}appointments`, config);
+};
+
+export const updateAppointment = async (id, body, token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  return await axios.put(`${root}appointments/${id}`, body, config);
+};
